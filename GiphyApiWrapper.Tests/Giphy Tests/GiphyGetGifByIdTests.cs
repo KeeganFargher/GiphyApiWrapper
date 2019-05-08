3e40101ebc557977a72088b7840f6b6704abce10
Keegan Fargher <fargherkeegan@gmail.com>
2019-05-08 11:08:05 +0200

    Tests

---
+++ GiphyApiWrapper.Tests/Giphy Tests/GiphyGetGifByIdTests.cs
@@ -0,0 +1,57 @@
+﻿using GiphyApiWrapper.Models;
+using GiphyApiWrapper.Tests.Mock_Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GiphyApiWrapper.Tests.Giphy_Tests
+{
+    public class GiphyGetGifByIdTests
+    {
+        [Fact]
+        public async Task GifIdIsNull_ThrowsFormatException()
+        {
+            var giphy = new Giphy("test");
+
+            string id = null;
+
+            _ = await Assert.ThrowsAsync<FormatException>(() => giphy.GetGifById(id));
+        }
+
+        [Fact]
+        public async Task GifIdIsEmpty_ThrowsFormatException()
+        {
+            var giphy = new Giphy("test");
+
+            string id = "";
+
+            _ = await Assert.ThrowsAsync<FormatException>(() => giphy.GetGifById(id));
+        }
+
+        [Fact]
+        public async Task NotSuccessStatusCode_ThrowsHttpRequestException()
+        {
+            var mockHttpHandler = HttpHandler.GetMockFailedHttpHandlerObject();
+            var giphy = new Giphy(mockHttpHandler);
+            var id = "xT4uQulxzV39haRFjG";
+
+            _ = await Assert.ThrowsAsync<HttpRequestException>(() => giphy.GetGifById(id));
+        }
+
+        [Fact]
+        public async Task WhenCalled_ReturnsRootObject()
+        {
+            var mockHttpHandler = HttpHandler.GetMockSuccessHttpHandlerObject();
+            var giphy = new Giphy(mockHttpHandler);
+            var id = "xT4uQulxzV39haRFjG";
+
+            var actual = await giphy.GetGifById(id);
+
+            Assert.NotNull(actual);
+            Assert.IsType<RootObject>(actual);
+        }
+    }
+}
---
+++ GiphyApiWrapper.Tests/Giphy Tests/GiphyStickerSearchTests.cs
@@ -0,0 +1,59 @@
+﻿using GiphyApiWrapper.Models;
+using GiphyApiWrapper.Models.Parameters;
+using GiphyApiWrapper.Tests.Mock_Data;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using GiphyApiWrapper;
+
+namespace GiphyApiWrapper.Tests.Giphy_Tests
+{
+    public class GiphyStickerSearchTests
+    {
+        [Fact]
+        public async Task StickerSearch_QueryNotSpecified_ThrowsFormatException()
+        {
+            var giphy = new Giphy("test");
+
+            //  The query variable was not set in the paramter model
+            //  which is required to run a query on giphy's api
+            var search = new StickerSearchParameter();
+
+            _ = await Assert.ThrowsAsync<FormatException>(() => giphy.StickerSearch(search));
+        }
+
+        [Fact]
+        public async Task StickerSearch_ParameterIsNull_ThrowsNullReferenceException()
+        {
+            var giphy = new Giphy("test");
+
+            StickerSearchParameter search = null;
+
+            _ = await Assert.ThrowsAsync<NullReferenceException>(() => giphy.StickerSearch(search));
+        }
+
+        [Fact]
+        public async Task StickerSearch_NotSuccessStatusCode_ThrowsHttpRequestException()
+        {
+            var mockHttpHandler = HttpHandler.GetMockFailedHttpHandlerObject();
+            var giphy = new Giphy(mockHttpHandler);
+            var search = new StickerSearchParameter { Query = "test" };
+
+            _ = await Assert.ThrowsAsync<HttpRequestException>(() => giphy.StickerSearch(search));
+        }
+
+        [Fact]
+        public async Task StickerSearch_WhenCalled_ReturnsRootObject()
+        {
+            var mockHttpHandler = HttpHandler.GetMockSuccessHttpHandlerObject();
+            var giphy = new Giphy(mockHttpHandler);
+            var search = new StickerSearchParameter { Query = "test" };
+
+            var actual = await giphy.StickerSearch(search);
+
+            Assert.NotNull(actual);
+            Assert.IsType<RootObject>(actual);
+        }
+    }
+}
